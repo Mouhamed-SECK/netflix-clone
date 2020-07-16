d23eaa5780809d302734824452ae12457f3b07b5
Mouhamed27-99 <medsoseck@gmail.com>
2020-07-17 00:05:20 +0200

    Styling our app and our Row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "../api/axios";
 
+import "./Row.css";
+
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-const Row = ({ title, fetchUrl }) => {
+const Row = ({ title, fetchUrl, isLargeRow }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -16,13 +18,19 @@ const Row = ({ title, fetchUrl }) => {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
-          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+          <img
+            className={`row__poster ${isLargeRow && "row__poster--large"}`}
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
         ))}
       </div>
     </div>
