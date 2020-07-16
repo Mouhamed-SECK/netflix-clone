2f00b35223471d5015111a61c958851e459b344c
Mouhamed27-99 <medsoseck@gmail.com>
2020-07-16 22:47:14 +0200

    Adding and setup axios in our project, Adding Row component and fetch all movies into it

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import "./App.css";
+import Row from "./components/Row";
 
-function App() {
-  return <div className="App"></div>;
-}
+import requests from "./api/request";
+
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App">
+      <h2>Netflix clone</h2>
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+      ></Row>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}></Row>
+    </div>
+  );
+};
 
 export default App;
